f8e268441f782f369a040bec0c3023337524d5e6
Noman Ahmed khan <128586752+NomanMediaMonitors@users.noreply.github.com>
2026-02-27 01:38:04 +0500

    update

--- Invoice-automation-V1/obj/Debug/net8.0/Invoice-automation-V1.AssemblyInfo.cs
+++ Invoice-automation-V1/obj/Debug/net8.0/Invoice-automation-V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Invoice-automation-V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bab74a749991c0e0ac149d67b86e3cddecdb2ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a69451831ecdc9bc2c1ef184f22b5452ed7250")]
 [assembly: System.Reflection.AssemblyProductAttribute("Invoice-automation-V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Invoice-automation-V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
